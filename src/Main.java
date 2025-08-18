cb01c8407f2cb443c403b2ced74bbd531c32b533
Chloe Nicole Monteza <cnmonteza64178@liceo.edu.ph>
2025-08-18 10:13:33 +0800

    revert main

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        String studentName = "John Smith";
-        double average = GradeCalculator.calculateAverage(85.5, 92.0, 78.5, 90.0);
-        String letterGrade = GradeCalculator.getLetterGrade(average);
-
-        GradeCalculator.displayResult(studentName, average);
-        System.out.println();
-        GradeCalculator.displayResult(studentName, average, letterGrade);
+        System.out.println("Main Branch!");
     }
 }
 
